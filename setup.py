34194f7a30a49d02c9d366bf209acb10b824f804
m-karakus <s.metinkarakus@gmail.com>
2024-11-28 02:13:01 +0300

    feat: github workflow added

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
-    ]
+    ],
     python_requires=">=3.8",
 )
