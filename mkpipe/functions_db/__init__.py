743d14ee4d5c55bad406ba3f8e5dd83bef3b93d0
m-karakus <s.metinkarakus@gmail.com>
2024-12-12 00:40:52 +0300

    Sqlite Support Added

--- mkpipe/functions_db/__init__.py
+++ mkpipe/functions_db/__init__.py
@@ -1,4 +1,4 @@
-from .postgres_connector import get_last_point, manifest_table_update, get_table_status
+from .connector import get_last_point, manifest_table_update, get_table_status
 
 __all__ = (
     get_last_point,
