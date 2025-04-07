bf8a8a5852f713422ec60ff4245221760edf05d3
m-karakus <s.metinkarakus@gmail.com>
2025-04-07 13:32:50 +0300

    feat:duckdb added

--- mkpipe/functions_db/register_db.py
+++ mkpipe/functions_db/register_db.py
@@ -1,5 +1,6 @@
 from .connector_postgresql import ConnectorPostgresql
 from .connector_sqlite import ConnectorSQLite
+from .connector_duckdb import ConnectorDuckDB
 
 DB_CONNECTIONS = {}
 
@@ -20,6 +21,11 @@ def connector_postgresql(connection_params):
 @register_db_connector('sqlite')
 def connector_sqlite(connection_params):
     return ConnectorSQLite(connection_params)
+
+
+@register_db_connector('duckdb')
+def connector_duckdb(connection_params):
+    return ConnectorDuckDB(connection_params)
 
 
 def get_db_connector(variant):
