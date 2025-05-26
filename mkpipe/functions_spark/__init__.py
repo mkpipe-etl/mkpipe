d01ad845c5309563061f82fc4e25d28b0b96ea24
m-karakus <s.metinkarakus@gmail.com>
2025-05-27 00:08:41 +0300

    feat: snowflake connections

--- mkpipe/functions_spark/__init__.py
+++ mkpipe/functions_spark/__init__.py
@@ -1,6 +1,7 @@
 from .register_file_parser import get_parser
 from .parquet_functions import remove_partitioned_parquet, read_schema, write_schema
 from .base_extractor import BaseExtractor
+from .base_loader import BaseLoader
 
 __all__ = (
     get_parser,
@@ -8,4 +9,5 @@ __all__ = (
     read_schema,
     write_schema,
     BaseExtractor,
+    BaseLoader,
 )
