da9250e7f6a9c0ce4d817fb8aec55aad78357f9f
m-karakus <s.metinkarakus@gmail.com>
2025-07-03 11:54:00 +0300

    feat: logger inside of the functions

--- mkpipe/functions_spark/session.py
+++ mkpipe/functions_spark/session.py
@@ -6,12 +6,23 @@ from ..plugins.registry_jar import collect_jars
 def create_spark_session(settings):
     jars = collect_jars()
 
+    # --- Safely append the flag to existing Java options ---
+    driver_java_options = (
+        f"-Duser.timezone={settings.timezone} "
+        "-XX:ErrorFile=/tmp/java_error%p.log "
+        "-XX:HeapDumpPath=/tmp "
+    )
+
+    executor_java_options = (
+        f"-Duser.timezone={settings.timezone} "
+    )
+
     conf = SparkConf()
     conf.setAppName(settings.driver_name)
     conf.setMaster('local[*]')
     conf.set('spark.driver.memory', settings.spark_driver_memory)
     conf.set('spark.executor.memory', settings.spark_executor_memory)
-    conf.set('spark.jars', jars)  # Distribute jars
+    conf.set('spark.jars', jars)
     conf.set('spark.driver.extraClassPath', jars)
     conf.set('spark.executor.extraClassPath', jars)
     conf.set('spark.network.timeout', '600s')
@@ -24,18 +35,16 @@ def create_spark_session(settings):
 
     # Dynamic allocation settings
     conf.set('spark.dynamicAllocation.enabled', 'true')
-    conf.set('spark.dynamicAllocation.minExecutors', '1')  # Minimum executors (adjustable)
-    conf.set('spark.dynamicAllocation.maxExecutors', '2')  # Maximum executors (adjustable)
-    conf.set('spark.dynamicAllocation.initialExecutors', '1')  # Starting number of executors
+    conf.set('spark.dynamicAllocation.minExecutors', '1')
+    conf.set('spark.dynamicAllocation.maxExecutors', '2')
+    conf.set('spark.dynamicAllocation.initialExecutors', '1')
     conf.set('spark.sql.session.timeZone', settings.timezone)
-    conf.set(
-        'spark.executor.extraJavaOptions',
-        f'-Duser.timezone={settings.timezone}',
-    )
-    conf.set(
-        'spark.driver.extraJavaOptions',
-        f'-Duser.timezone={settings.timezone} -XX:ErrorFile=/tmp/java_error%p.log -XX:HeapDumpPath=/tmp',
-    )
 
+    # Set the updated Java options
+    conf.set('spark.driver.extraJavaOptions', driver_java_options)
+    conf.set('spark.executor.extraJavaOptions', executor_java_options)
+
+    # Build the SparkSession
     spark = SparkSession.builder.config(conf=conf).getOrCreate()
+
     return spark
