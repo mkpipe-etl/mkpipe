e206e7ffa3b631f332d45c059913bcde2ed75976
m-karakus <s.metinkarakus@gmail.com>
2025-07-28 01:12:01 +0300

    feat: backend functions update

--- mkpipe/functions_db/connector_clickhouse.py
+++ mkpipe/functions_db/connector_clickhouse.py
@@ -60,7 +60,7 @@ class ConnectorClickhouse:
                 error_message Nullable(String),
                 updated_time DateTime DEFAULT now()
             ) ENGINE = MergeTree()
-            ORDER BY (pipelinename,table_name);
+            ORDER BY (pipe_linename, table_name);
         """)
 
         query_result = client.query(
